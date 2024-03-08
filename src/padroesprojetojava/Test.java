0b3e2a5611668827176b849d84dffb15ecda094b
amandaestevez <amandaestevez8@hotmail.com>
2024-03-08 05:54:36 -0300

    Commit Final 1.1 - 08/03/2024 - Adiciona comentários em cada teste

--- src/padroesprojetojava/Test.java
+++ src/padroesprojetojava/Test.java
@@ -2,16 +2,20 @@ package padroesprojetojava;
 
 public class Test {
     public static void main(String[] args) {
+
+        //Teste para verificar o funcionamento do Singleton Lazy
         SingletonLazy lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         lazy = SingletonLazy.getInstancia();
         System.out.println(lazy);
         
+        //Teste para verificar o funcionamento do Singleton Eager
         SingletonEager eager = SingletonEager.getInstancia();
         System.out.println(eager);
         eager = SingletonEager.getInstancia();
         System.out.println(eager);
 
+        //Teste para verificar o funcionamento do Singleton Lazy Holder
         SingletonLazyHolder lazyHolder = SingletonLazyHolder.getInstancia();
         System.out.println(lazyHolder);
         lazyHolder = SingletonLazyHolder.getInstancia();
