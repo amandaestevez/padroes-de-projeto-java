01acf146460cf3affbcd7efdb2a3dc398e503cf4
amandaestevez <amandaestevez8@hotmail.com>
2024-03-12 06:35:00 -0300

    Commit Final Facade 12/03/2024 | Teste realizado e padrão funciona

---
+++ src/padroesprojetojava/facade/Test.java
@@ -0,0 +1,12 @@
+package padroesprojetojava.facade;
+
+import padroesprojetojava.facade.Facade;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Amanda", "11111-111");
+    }
+
+}
--- src/padroesprojetojava/facade/facade.java
+++ src/padroesprojetojava/facade/facade.java
@@ -1,7 +1,18 @@
 package padroesprojetojava.facade;
 
+import subsistema1.crm.CrmService;
+import subsistema2.cep.CepApi;
+
 public class Facade {
     
     public void migrarCliente(String nome, String cep) {
+        String cidade = CepApi.getInstancia().recuperarCidade(cep);
+        String estado = CepApi.getInstancia().recuperarEstado(cep);
+
+        CrmService.gravarCliente(nome, cep, cidade, estado);
     }
 }
+
+/*This code snippet is a Java implementation of the Facade design pattern. 
+ * It provides a simplified interface for interacting with the subsystems "CrmService" and "CepApi" by encapsulating their functionalities behind a single method "migrarCliente". 
+ * This method takes in a client's name and zip code, retrieves the city and state information using the CepApi, and then uses the CrmService to record the client's information. */
